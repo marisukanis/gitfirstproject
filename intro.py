a7f1cfd713e39cc4d8681dfec0a390f28a55ebf8
marisukanis <110673474+marisukanis@users.noreply.github.com>
2022-08-08 22:41:10 +0300

    Python intro course materials

---
+++ intro.py
@@ -0,0 +1,33 @@
+print(2+9)
+
+print("welcome to class")
+print('It is another day of learning')
+print("It's a great day")
+
+# is used for commenting in python
+"""
+    since python ignores any string that isn't assigned to a variable,
+    the tripe quote can be used for commenting
+"""
+
+"""
+Variable is simply a container for storing a value
+- name
+- value
+- type (based on the value)
+- created at the first assignment of a value
+
+Rules:
+- change the value of a variable assigning a new value
+- name only contains letters a-zA-z, numbers 0-9 and _ symbol
+- it is case sensitive Apple != apple
+- it cannot start with a digit
+"""
+name = "Maris"
+age = 28
+Name = "Ukanis"
+
+print(name + " " + Name)
+name = "Marisukanis"
+print(name + " " + Name)
+print(type(name))
